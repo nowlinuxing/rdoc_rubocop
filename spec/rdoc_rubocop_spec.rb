91c5b3f42fb936a018812501033b07d9633939eb
Loose Coupling <loosecpl@gmail.com>
2018-12-18 06:31:34 +0900

    Remove unnecessary spec

--- spec/rdoc_rubocop_spec.rb
+++ spec/rdoc_rubocop_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe RDocRuboCop do
   it "has a version number" do
     expect(RDocRuboCop::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
